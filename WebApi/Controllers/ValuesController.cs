8b16989cd619d274cab09fe1129c2497b9a5e062
Goodson Nyasha Mutongi <goodnyasha@yahoo.co.uk>
2020-06-15 12:23:32 +0200

    Add project files.

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Api Data 61", "Api Data 2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "Api Data";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
